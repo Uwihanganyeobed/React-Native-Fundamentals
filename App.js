75460724f492de03579de2959050e70e5a550bce
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-21 10:50:53 +0200

    adding data

--- App.js
+++ App.js
@@ -1,32 +1,122 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import CourseListScreen from "./Screens/CourseListScreen";
-import ProfileScreen from "./Screens/ProfileScreen";
-import SettingScreen from "./Screens/SettingScreen";
-import Ionicons from '@expo/vector-icons/Ionicons'
-import {AboutStack} from './AppStack'
-const Tab=createBottomTabNavigator()
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
+  Platform
+} from "react-native";
+import Cart from "./Components/Core-Components/Cart";
+import Box from "./Components/RNLayout/Box";
+import Greet from "./Components/Core-Components/Greet";
+import StyleSheetAPI from "./Components/styleSheetAPI/StyleSheetAPI";
+import UIDynamic from "./Components/DynamicUI/UIDynamic";
+import UIWindow from "./Components/DynamicUI/UIWindow";
+import CustomButton from "./Components/DynamicUI/customButton/customButton.android";
+const logoImage = require("./assets/adaptive-icon.png");
+export default function App() {
+  const [count, setCount]=useState(0)
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+    <View style={{backgroundColor: 'plum', flex: 1}}>
+    {/* <StyleSheetAPI/> */}
+    {/* <UIDynamic/> */}
+    {/* <UIWindow/> */}
+    <CustomButton title="press me"onPress={()=>alert('pressed')}/>
+    </View>
+    </SafeAreaView>
+  );
+}
+const styles=StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: 'plum'
+  },
+  container: {
+    flex: 1,
+    // // height: 300,
+    // // flexGrow: 1,
+    // // flexWrap: 'wrap',
+    // // rowGap: 20,
+    // // columnGap: 30,
+    // // gap: 20,
+    // // alignContent: 'space-around' ,
+    // marginTop: 30,
+    // borderWidth: 6,
+    // // flexDirection: 'row',
+    // // justifyContent: "flex-end",
+    // // alignItems: 'baseline',
+    // borderColor: 'lightblue'
+  }
+})
 
-export default function App(){
-   return(
-      <NavigationContainer>
-         <Tab.Navigator screenOptions={{
-            tabBarLabelPosition: 'below-icon',
-            tabBarShowLabel: true,
-            tabBarActiveTintColor: 'purple',
-            // tabBarInactiveTintColor
-         }
-         }>
-            <Tab.Screen name="Course List"component={CourseListScreen} />
-            <Tab.Screen name="Profile"component={ProfileScreen} options={{
-               tabBarLabel: 'My Profile',
-               tabBarIcon: ({color})=><Ionicons name="person"size={20} color={color}/>,
-               tabBarBadge: 3,
-            }} />
-            <Tab.Screen name="Settings"component={SettingScreen} />
-            <Tab.Screen name="AboutStack"component={AboutStack} options={{
-               headerShown: false
-            }} />
-         </Tab.Navigator>
-      </NavigationContainer>
-   )}
+{
+  /* <ScrollView>
+<Button
+    title="Press"
+    onPress={() => alert("Button Pressed success")}
+    color="midnightblue"
+    // disabled
+  />
+  <Pressable onPress={()=>alert('Image pressed')}>
+  <Image source={logoImage} style={{ width: 300, height: 300 }} />
+  </Pressable>
+  <Pressable onPress={()=>alert('Text pressed')}>
+  <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+    fugiat unde maxime sit magnam dicta error nobis nihil qui esse
+    placeat, recusandae nemo amet alias. Asperiores quibusdam quam
+    dignissimos ab. Lorem ipsum dolor sit amet consectetur adipisicing
+    elit. Aperiam fugiat unde maxime sit magnam dicta error nobis nihil
+    qui esse placeat, recusandae nemo amet alias. Asperiores quibusdam
+    quam dignissimos ab.
+  </Text>
+  <Image source={logoImage} style={{ width: 300, height: 300 }} />
+  </Pressable>
+
+</ScrollView> */
+}
+
+{
+  /* <Button
+title="Press"
+onPress={() => setIsModalVisible(true)}
+color="midnightblue"
+/>
+<Modal
+visible={isModalVisible}
+onRequestClose={() => setIsModalVisible(false)}
+animationType="slide"
+presentationStyle="pageSheet"
+>
+<View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+  <Text>Modal Content</Text>
+  <Button
+    title="Close"
+    color="red"
+    onPress={() => setIsModalVisible(false)}
+  />
+</View>
+</Modal> */
+}
+
+    // <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+    //   <Greet name='Vargas'/>
+    //   <Greet name='Obed'/>
+    //   <View>
+    //     <Cart count={count}/>
+    //     <Button title="Increment Cart" onPress={()=>setCount(count+1)}  />
+    //     <Button title="Decrement Cart" onPress={()=>count>0 && setCount(count-1)} />
+    //     <Button title="Reset Cart" onPress={()=>setCount(0)} />
+    //   </View>
+    // </View>
